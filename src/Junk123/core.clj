db6acb5d2891ea0a8c41735e2b434c36f33d33df
Kei Suzuki <ksdevx@gmail.com>
2011-02-27 08:48:34 +0900

    Initial commit

---
+++ src/Junk123/core.clj
@@ -0,0 +1,1 @@
+(ns Junk123.core)
---
+++ test/Junk123/test/core.clj
@@ -0,0 +1,6 @@
+(ns Junk123.test.core
+  (:use [Junk123.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
